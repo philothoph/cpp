c8a93ba336d1ddbde880d66d61b30ffb0387a345
philothoph <dmitry@lukyanov.us>
2015-12-27 17:34:29 +0300

    Complete 4.11.cpp

---
+++ 4.11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a, b, c, d;
+
+    if (a > b && b > c && c > d)
+    {
+        /*code*/
+    }
+
+    return 0;
+}
