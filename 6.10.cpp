8f24adb30c6e41e34a03f28e1453d3b3e09c34d3
philothoph <dmitry@lukyanov.us>
2016-03-14 14:51:38 +0300

    Complete 6.10 - 6.17

---
+++ 6.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout; using std::endl;
+
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int main()
+{
+    int y = 43, sd = 2;
+
+    swap(&y, &sd);
+    cout << "y = " << y
+         << "\nsd = " << sd
+         << endl;
+
+    return 0;
+}
